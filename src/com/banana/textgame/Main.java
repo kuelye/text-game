ff1afdf79139bc806373c0bcf03793b64dd877db
Alan Turing <alanturing@enigma.org>
2017-10-23 16:14:35 +0300

    test commit

--- src/com/banana/textgame/Main.java
+++ src/com/banana/textgame/Main.java
@@ -36,7 +36,7 @@ public class Main {
 
         System.out.println("Как тебе зовут?");
         String имя = клавиатура.nextLine();
-        System.out.println("Привет, " + имя);
+        System.out.println("Здравствуй, " + имя);
     }
 
     /*
