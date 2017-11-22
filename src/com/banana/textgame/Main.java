8fee779f519eade2591c2aeff4e2dea14abf87de
Alexey Leshchuk <kuelye@gmail.com>
2017-11-23 00:07:28 +0300

    lesson 09 changes

--- src/com/banana/textgame/Main.java
+++ src/com/banana/textgame/Main.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+class Main {
+
+    // ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
+
+    Scanner keyboard = new Scanner(System.in);
+    String[] доступныеЯзыки = {"Java", "Python", "Kotlin", "Pascal", "Ada", "C", "JavaScript"};
+    User пользователь = new User();
 
     /*
      * Главный метод.
@@ -38,29 +44,20 @@ public class Main {
         System.out.println("Здравствуй, " + имя);
     }
 
-    // ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
-
-    int dollars = 0;
-    int mood = 3;
-    String[] доступныеЯзыки = {"Java", "Python", "Kotlin", "Pascal", "Ada", "C", "JavaScript"};
-    boolean[] изученныеЯзыки = {true, false, false, false, false, false, false};
-    Scanner keyboard = new Scanner(System.in);
-    ArrayList<String> компании = new ArrayList<>();
-
     /*
      * Метод вызывается каждый игровый день.
      * Единственный параметр: dayNumber - номер текущего игрового дня.
      */
     void onNewDay(int dayNumber) {
         System.out.println("День номер " + dayNumber + ".");
-        System.out.println("Ваш счёт: " + dollars + "$.");
+        System.out.println("Ваш счёт: " + пользователь.dollars + "$.");
         String smiles = "";
-        for (int i = 1; i <= mood; i = i + 1) {
+        for (int i = 1; i <= пользователь.mood; i = i + 1) {
             smiles = smiles + "☺";
         }
         System.out.println("Ваше настроение: " + smiles + ".");
         напечатайИзученныеЯзыки();
-        System.out.println("Компании, в которых вы работаете: " + компании + ".");
+        System.out.println("Компании, в которых вы работаете: " + пользователь.компании + ".");
 
         System.out.println("Чего делать будем?");
         String action = keyboard.nextLine();
@@ -75,8 +72,8 @@ public class Main {
                 // пишём код
                 System.out.println("Ваш код на сегодня:");
                 String код = keyboard.nextLine();
-                dollars = dollars + код.length();
-                mood = mood - 1;
+                пользователь.dollars = пользователь.dollars + код.length();
+                пользователь.mood = пользователь.mood - 1;
                 break;
 
             case "пельмешки":
@@ -85,7 +82,7 @@ public class Main {
             case "скакалка":
                 // прыгаем на скакалке
                 System.out.println("Окей, вы попрыгали на скакалке.");
-                mood = mood + 2;
+                пользователь.mood = пользователь.mood + 2;
                 break;
             case "играть":
                 броситьКость();
@@ -111,15 +108,15 @@ public class Main {
 
         for (int i = 0; i < доступныеЯзыки.length; i++) {
             if (доступныеЯзыки[i].equals(выбранныйЯзык)) {
-                if ((изученныеЯзыки[i] == false) && mood > 0 && dollars >= 20) {
-                    изученныеЯзыки[i] = true;
-                    dollars -= 20;
-                    mood -= 1;
-                } else if (mood == 0) {
+                if ((пользователь.изученныеЯзыки[i] == false) && пользователь.mood > 0 && пользователь.dollars >= 20) {
+                    пользователь.изученныеЯзыки[i] = true;
+                    пользователь.dollars -= 20;
+                    пользователь.mood -= 1;
+                } else if (пользователь.mood == 0) {
                     System.out.println("Вам слишком грустно.");
-                } else if (dollars < 20) {
+                } else if (пользователь.dollars < 20) {
                     System.out.println("У вас совсем нет денег.");
-                } else if (изученныеЯзыки[i] == true) {
+                } else if (пользователь.изученныеЯзыки[i] == true) {
                     System.out.println("Вы уже знаете этот язык.");
                 }
 
@@ -134,8 +131,8 @@ public class Main {
 
     void напечатайИзученныеЯзыки() {
         System.out.println("Вы знаете языки:");
-        for (int i = 0; i < изученныеЯзыки.length; i++) {
-            if (изученныеЯзыки[i] == true) {
+        for (int i = 0; i < пользователь.изученныеЯзыки.length; i++) {
+            if (пользователь.изученныеЯзыки[i] == true) {
                 System.out.println(" • " + доступныеЯзыки[i]);
             }
         }
@@ -157,14 +154,14 @@ public class Main {
     }
 
     void скушатьПельмешки(int количествоПельмешек) {
-        dollars = dollars - 5 * количествоПельмешек;
-        mood = mood + 5 * количествоПельмешек;
+        пользователь.dollars = пользователь.dollars - 5 * количествоПельмешек;
+        пользователь.mood = пользователь.mood + 5 * количествоПельмешек;
     }
 
     int верниФинальныеОчки () {
-        int очки = dollars * 2 + mood * 6;
-        for (int i = 0; i < изученныеЯзыки.length; i++) {
-            if (изученныеЯзыки[i] == true) {
+        int очки = пользователь.dollars * 2 + пользователь.mood * 6;
+        for (int i = 0; i < пользователь.изученныеЯзыки.length; i++) {
+            if (пользователь.изученныеЯзыки[i] == true) {
                 очки = очки + 4;
 
             }
@@ -175,17 +172,17 @@ public class Main {
     void найтиРаботу() {
         System.out.println("В какой компании будете работать?");
         String компания = keyboard.nextLine();
-        компании.add(компания);
+        пользователь.компании.add(компания);
     }
 
     void броситьКость() {
         int число1 = (int) (Math.random() * 6 + 1);
         int число2 = (int) (Math.random() * 6 + 1);
         System.out.println("Вам выпали: " + String.valueOf(число1) + " и " + String.valueOf(число2) + ".");
-        dollars -= 1;
+        пользователь.dollars -= 1;
         if (Math.max(число1, число2) % 2 == 0) {
             System.out.println("Ура, вы выиграли!");
-            dollars += 5;
+            пользователь.dollars += 5;
         }
     }
 
---
+++ src/com/banana/textgame/User.java
@@ -0,0 +1,12 @@
+package com.banana.textgame;
+
+import java.util.ArrayList;
+
+public class User {
+
+    int dollars = 0;
+    int mood = 3;
+    boolean[] изученныеЯзыки = {true, false, false, false, false, false, false};
+    ArrayList<String> компании = new ArrayList<>();
+
+}
